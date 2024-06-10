e650fce370d04881b1b600ed22839710633d23c0
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-06-10 07:50:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Created by Tivotal */
+
+let showBtn = document.querySelector(".show-btn");
+let popup = document.querySelector(".popup");
+let closeBtn = document.querySelector(".close-btn");
+
+showBtn.addEventListener("click", () => {
+  popup.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.classList.remove("show");
+});
